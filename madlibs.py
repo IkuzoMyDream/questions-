95550b9bfc9f72bd30314354ca70baf554df6b21
IkuzoMyDream <96933734+IkuzoMyDream@users.noreply.github.com>
2022-03-28 21:55:04 +0700

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# # 1 madlips
+# try:
+#  name1 = input('name1: ')
+#  name2 = input('name2: ')
+#  name3 = input('name3: ')
+#  favnum = int(input('fav num :'))
+#
+#  madlibs = f'today{name1} gonna show you how great i\'m, first of all, let me introduce myself, myname is {name2},  ' \
+#           f'my fathers name is {name3} and i never gonna give you up never gonna let you down ohoh you failed man ' \
+#           f'haha it\' its ok to tell me somethings you did\'nt expect that has been ok man i love you {favnum} bye   '
+#  print(madlibs)
+# except:
+#     print('ErRoR iNpUt')
+
+# 2 
+
+
