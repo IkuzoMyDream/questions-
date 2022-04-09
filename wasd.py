df56e11d728bd410f9219b47a0a503e690732090
IkuzoMyDream <96933734+IkuzoMyDream@users.noreply.github.com>
2022-04-09 15:39:28 +0700

    Create wasd.py

---
+++ wasd.py
@@ -0,0 +1,42 @@
+n1= input()
+n2= input()
+n3= input()
+n4= input()
+# n1
+if n1 == 'w':
+    print('^')
+elif n1 == 'a':
+    print('<')
+elif n1 == 's':
+    print('v')
+else:
+    print('>')
+
+if n2 == 'w':
+    print('^')
+elif n2 == 'a':
+    print('<')
+elif n2 == 's':
+    print('v')
+else:
+    print('>')
+
+if n3 == 'w':
+    print('^')
+elif n3 == 'a':
+    print('<')
+elif n3 == 's':
+    print('v')
+else:
+    print('>')
+
+if n4 == 'w':
+    print('^')
+elif n4 == 'a':
+    print('<')
+elif n4 == 's':
+    print('v')
+else:
+    print('>')
+
+
